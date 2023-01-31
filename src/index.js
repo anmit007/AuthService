f43a67e0bd8c5863ccce10d5435045105b4fc207
AnmZ <anmit.ray911@gmail.com>
2023-01-31 19:50:12 +0530

    Added JWT create and verify

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const {PORT} = require('./config/server-config');
 const apiRoutes = require('./routes/index');
 const app =  express();
 const UserService = require('./services/user-service');
-
+const db = require('./models/index');
 const prepareAndStartServer = () =>
 {
     app.use(bodyParser.json());
@@ -12,6 +12,10 @@ const prepareAndStartServer = () =>
     app.use('/api',apiRoutes);
     app.listen(PORT,async ()=>{
         console.log(`SERVER STARTED ON PORT :  ${PORT}`);
+        // if(process.env.DB_SYNC)
+        // {
+        //     db.sequelize.sync({alter : false});
+        // }
     })
 }
 prepareAndStartServer();
--- src/models/user.js
+++ src/models/user.js
@@ -6,13 +6,16 @@ const {SALT} = require('../config/server-config');
 const bcrypt =require('bcrypt');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
+    /*
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
-     */
+    */
     static associate(models) {
       // define association here
+      this.belongsToMany(models.Role,{
+        through : 'User_Roles',
+      })
     }
   }
   User.init({
